62ce7c392221805f5eeecb71538b9c1c6619a188
Hong-Sovannarith <sovannarith1293@gmail.com>
2026-01-10 20:29:48 +0530

    Add files via upload

---
+++ FirstSpring/src/main/java/com/example/FirstSpring/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.FirstSpring;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public HelloResponse hello() {
+        return new HelloResponse("Hello world");
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name) {
+        return "Hello " + name + "!";
+    }
+}
